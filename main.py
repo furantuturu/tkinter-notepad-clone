31fb93f4a51981fef1305a188305f1bbef228b72
Haemoffy <fuwansuu@gmail.com>
2024-02-02 16:31:17 +0800

    v1.02 added horizontal scrollbar for word wrap toggle adjustment of layouts (status bar frame row and horizontal scrollbar to not collide)

--- main.py
+++ main.py
@@ -16,18 +16,22 @@ window.grid_columnconfigure(0, weight=1)
 
 textarea = Text(master=window, undo=True, height=window_h, font=('Arial', 16))
 textarea.grid(row=0, column=0, sticky=EW)
-scrollbar = Scrollbar(master=window, orient=VERTICAL, command=textarea.yview)
-scrollbar.grid(row=0, column=1, sticky=NS)
-textarea.config(yscrollcommand=scrollbar.set)
+
+scrollbar_y = Scrollbar(master=window, orient=VERTICAL, command=textarea.yview)
+scrollbar_x = Scrollbar(master=window, orient=HORIZONTAL, command=textarea.xview)
+
+scrollbar_y.grid(row=0, column=1, sticky=NS)
+
+textarea.config(xscrollcommand=scrollbar_x.set, yscrollcommand=scrollbar_y.set)
 
 status_bar_frame = Frame(master=window, height=24)
-Label(master=status_bar_frame, width=24).grid(row=1, column=0)
-Label(master=status_bar_frame, text="Ln 1, Col 1", relief=GROOVE, justify=LEFT, anchor=W).grid(row=1, column=1, ipadx=40)
-Label(master=status_bar_frame, text="100%", justify=LEFT, anchor=W).grid(row=1, column=2, ipadx=5)
-Label(master=status_bar_frame, text="Windows (CRLF)", relief=GROOVE, justify=LEFT, anchor=W).grid(row=1, column=3, ipadx=10)
-Label(master=status_bar_frame, text="UTF-8", justify=LEFT, anchor=W).grid(row=1, column=4, ipadx=30)
+Label(master=status_bar_frame, width=24).grid(row=2, column=0)
+Label(master=status_bar_frame, text="Ln 1, Col 1", relief=GROOVE, justify=LEFT, anchor=W).grid(row=2, column=1, ipadx=40)
+Label(master=status_bar_frame, text="100%", justify=LEFT, anchor=W).grid(row=2, column=2, ipadx=5)
+Label(master=status_bar_frame, text="Windows (CRLF)", relief=GROOVE, justify=LEFT, anchor=W).grid(row=2, column=3, ipadx=10)
+Label(master=status_bar_frame, text="UTF-8", justify=LEFT, anchor=W).grid(row=2, column=4, ipadx=30)
 
-menutab = menus.Menus(window=window, textarea=textarea, status_bar_frame=status_bar_frame)
+menutab = menus.Menus(window=window, textarea=textarea, status_bar_frame=status_bar_frame, scrollbar_x=scrollbar_x)
 
 if __name__ == "__main__":
     window.mainloop()
