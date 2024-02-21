17e0dbb8b949df34c52f84150eb6276fa52517e4
Haemoffy <fuwansuu@gmail.com>
2024-02-21 14:08:48 +0800

    v1.05 apply changes of the changed class and file name removed font object and relocated it to the UI class removed textareaframe object since its now gone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from tkinter import *
-from tkinter import font
-import menus
-import textareaframe
+import notepadUI
 
 window = Tk()
 window_w = 727
@@ -17,9 +15,6 @@ window.geometry(f"{window_w}x{window_h}+{x}+{y}")
 window.grid_rowconfigure(0, weight=1)
 window.grid_columnconfigure(0, weight=1)
 
-font = font.Font(family='Arial', size=16)
-
 if __name__ == "__main__":
-    textframe = textareaframe.TextFrame(window=window, default_font=font)
-    menutab = menus.Menus(window=window, textarea=textframe.textarea, font=font ,status_bar_frame=textframe.status_bar_frame, scrollbar_x=textframe.scrollbar_x)
+    menutab = notepadUI.UI(window=window)
     window.mainloop()
